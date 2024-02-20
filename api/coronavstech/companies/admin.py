f6d42c46672a8768879d6bebd737413d41a121d8
Quy Nguyen <quy.nguyen@wdc.com>
2024-02-20 11:52:52 -0800

    create main branch empty

--- api/coronavstech/companies/admin.py
+++
@@ -1,8 +0,0 @@
-from django.contrib import admin
-
-from api.coronavstech.companies.models import Company
-
-
-@admin.register(Company)
-class CompanyAdmin(admin.ModelAdmin):
-    pass
--- api/coronavstech/companies/serializers.py
+++
@@ -1,8 +0,0 @@
-from rest_framework import serializers
-from api.coronavstech.companies.models import Company
-
-
-class CompanySerializer(serializers.ModelSerializer):
-    class Meta:
-        model = Company
-        fields = ["id", "name", "status", "application_link", "last_update", "notes"]
